9dfba803ef22dcefd1020b653cc1d084561da027
Anduin Xue <xuef@microsoft.com>
2020-08-14 14:02:12 +0800

    Auto stop sql server.

--- mssql/config_password.sh
+++ mssql/config_password.sh
@@ -6,6 +6,7 @@ config_password()
         echo "You must specify a new dbPassword!";
         return 9;
     fi;
+    systemctl stop mssql-server.service
     MSSQL_SA_PASSWORD=$dbPassword MSSQL_PID='express' /opt/mssql/bin/mssql-conf -n setup accept-eula
     systemctl restart mssql-server
 }
