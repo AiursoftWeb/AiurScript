97ac7048216bdd0d4e83209a907031420c9ed6d5
Anduin Xue <xuef@microsoft.com>
2020-08-14 16:16:03 +0800

    Hide dotnet telementry.

--- install/dotnet.sh
+++ install/dotnet.sh
@@ -1,4 +1,4 @@
 wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb && rm ./packages-microsoft-prod.deb
 apt update
-apt install -y apt-transport-https dotnet-sdk-3.1 libgdiplus
+DOTNET_CLI_TELEMETRY_OPTOUT=1 DOTNET_PRINT_TELEMETRY_MESSAGE="false" apt install -y apt-transport-https dotnet-sdk-3.1 libgdiplus
