cfc3373260ba6b6f2a3d7aab5d394b1b29a71cbf
Anduin Xue <anduin@aiursoft.com>
2020-08-14 13:15:55 +0800

    Update open_port.sh

--- Firewall/open_port.sh
+++ Firewall/open_port.sh
@@ -1,11 +1,11 @@
 open_port()
 {
-    port_to_open="$1"
-    if [[ "$port_to_open" == "" ]]; then
-        echo "You must specify a port!"
-        return 9
-    fi
-
-    ufw allow $port_to_open/tcp
-    ufw reload
+    port_to_open="$1";
+    if [[ "$port_to_open" == "" ]]; 
+    then
+        echo "You must specify a port!";
+        return 9;
+    fi;
+    ufw allow $port_to_open/tcp;
+    ufw reload;
 }
