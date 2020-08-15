97fc5fe3556f3ff50294c82f436baf3040ff8210
Anduin Xue <xuef@microsoft.com>
2020-08-15 14:00:34 +0800

    sql server install better.

--- install/sql_server.sh
+++ install/sql_server.sh
@@ -2,4 +2,5 @@ curl -s https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 add-apt-repository "$(curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/mssql-server-2019.list)"
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 apt update
-ACCEPT_EULA=Y apt install -y apt-transport-https mssql-server mssql-tools unixodbc-dev ufw libgdiplus
+ACCEPT_EULA=Y apt install -y apt-transport-https mssql-server mssql-tools unixodbc-dev
+sleep 5 #wait for its start.
