ed35231a76b9b62b0e3c4b8cd9f76620e2b8a5b2
Anduin Xue <xuef@microsoft.com>
2020-08-24 17:55:38 +0800

    Aiurui support cors.

--- caddy/add_file.sh
+++ caddy/add_file.sh
@@ -3,8 +3,16 @@ add_caddy_file()
     domain_name="$1"
     path="$2"
     cat /etc/caddy/Caddyfile | grep -q "an easy way" && echo "" > /etc/caddy/Caddyfile
+    cat /etc/caddy/Caddyfile | grep -q "Access-Control-Allow-Origin" || echo "
+(cors) {
+    @origin header Origin {args.0}
+    header @origin Access-Control-Allow-Origin \"{args.0}\"
+    header @origin Access-Control-Request-Method GET
+}
+    " > /etc/caddy/Caddyfile
     echo "
 $domain_name {
+    import cors *;
     root * $path
     file_server
 }" >> /etc/caddy/Caddyfile
