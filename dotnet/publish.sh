f480817f7e831f4f1f2384dc59c0233319619f8f
Anduin Xue <xuef@microsoft.com>
2020-08-24 12:02:42 +0800

    Fix dotnet install script.

--- dotnet/publish.sh
+++ dotnet/publish.sh
@@ -2,7 +2,7 @@ publish()
 {
     output="$1"
     csproj="$2"
-    ls | grep -q obj || dotnet restore $csproj -r Linux-x64
+    ls | grep -q obj || dotnet restore $csproj -r linux-x64
     dotnet publish -r linux-x64 -c Release --no-self-contained --no-restore -o $output /p:PublishReadyToRun=true $csproj
 }
 
