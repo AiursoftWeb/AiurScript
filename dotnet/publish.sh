4b7a9043950fd8c68dbc0df25164b6d38a6543c0
Anduin Xue <xuef@microsoft.com>
2020-08-24 10:58:57 +0800

    Optional dotnet restore.

--- dotnet/publish.sh
+++ dotnet/publish.sh
@@ -2,8 +2,8 @@ publish()
 {
     output="$1"
     csproj="$2"
-
-    dotnet publish -r linux-x64 -c Release --no-self-contained -o $output /p:PublishReadyToRun=true $csproj
+    ls | grep -q obj || dotnet restore $csproj
+    dotnet publish -r linux-x64 -c Release --no-self-contained --no-restore -o $output /p:PublishReadyToRun=true $csproj
 }
 
 publish "$@"
--- install/dotnet.sh
+++ install/dotnet.sh
@@ -2,3 +2,4 @@ wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/packages-
 dpkg -i packages-microsoft-prod.deb && rm ./packages-microsoft-prod.deb
 apt update
 DOTNET_CLI_TELEMETRY_OPTOUT=1 DOTNET_PRINT_TELEMETRY_MESSAGE="false" apt install -y apt-transport-https dotnet-sdk-3.1 libgdiplus
+dotnet tool install --global dotnet-ef
