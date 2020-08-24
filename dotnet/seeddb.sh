c46e549784ffdf5c37008e98cd065f1f1a979eb6
Anduin Xue <xuef@microsoft.com>
2020-08-24 19:49:06 +0800

    seeddb method.

---
+++ dotnet/seeddb.sh
@@ -0,0 +1,14 @@
+seedDb()
+{
+    path="$1"
+    db_name="$2"
+    dbPassword="$3"
+    connectionString="Server=tcp:127.0.0.1,1433;Database=$db_name;uid=sa;Password=$dbPassword;MultipleActiveResultSets=True;"
+    cp -n $path/appsettings.json $path/appsettings.Production.json
+    aiur text/edit_json "ConnectionStrings.DatabaseConnection" "$connectionString" $path/appsettings.Production.json
+    aiur text/edit_json "ConnectionStrings.DatabaseConnection" "$connectionString" $path/appsettings.json
+    dotnet ef database update --project $path
+    dotnet add $path package Microsoft.EntityFrameworkCore.Design
+}
+
+seedDb "$@"
