309d532dcb62c8eb4495a7abc13d74e4e9ddc350
Anduin Xue <xuef@microsoft.com>
2020-08-25 23:46:55 +0800

    Allow cache for files.

--- caddy/add_file.sh
+++ caddy/add_file.sh
@@ -3,16 +3,10 @@ add_caddy_file()
     domain_name="$1"
     path="$2"
     cat /etc/caddy/Caddyfile | grep -q "an easy way" && echo "" > /etc/caddy/Caddyfile
-    cat /etc/caddy/Caddyfile | grep -q "Access-Control-Allow-Origin" || echo "
-(cors) {
-    @origin header Origin {args.0}
-    header @origin Access-Control-Allow-Origin \"{args.0}\"
-    header @origin Access-Control-Request-Method GET
-}
-    " > /etc/caddy/Caddyfile
     echo "
 $domain_name {
-    import cors *;
+    header Access-Control-Allow-Origin *
+    header Cache-Control \"public, max-age=604800\"
     root * $path
     file_server
 }" >> /etc/caddy/Caddyfile
