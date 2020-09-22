2475b095286cf6ba20bc893fe94fb740e26b1b70
Anduin Xue <anduin@aiursoft.com>
2020-09-22 13:11:45 +0800

    Support custom branch name.

--- git/clone_to.sh
+++ git/clone_to.sh
@@ -2,9 +2,19 @@ clone_to()
 {
     repo="$1"
     path="$2"
+    branch_name=$3;
+
+    if [[ $branch_name == "" ]];
+    then
+        branch_name="master"
+        echo "Using branch name: $branch_name"
+    else
+        echo "Branch name is: $branch_name"
+    fi
+    
     _=$(git --help || apt install git -y)
     _=$(ls $path && rm $path -rvf)
-    git clone -b master https://github.com/$repo.git $path
+    git clone -b $branch_name https://github.com/$repo.git $path
 }
 
 clone_to "$@"
