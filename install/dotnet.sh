a651ad0fadb6de97ca71a6e766da7dc41a05d924
Anduin Xue <anduin@aiursoft.com>
2020-11-12 12:43:14 +0800

    Update dotnet.sh

--- install/dotnet.sh
+++ install/dotnet.sh
@@ -1,5 +1,5 @@
 wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb && rm ./packages-microsoft-prod.deb
-apt update
+apt-get update
 DOTNET_CLI_TELEMETRY_OPTOUT=1 DOTNET_PRINT_TELEMETRY_MESSAGE="false" apt install -y apt-transport-https dotnet-sdk-5.0 libgdiplus
-dotnet tool install --global dotnet-ef
+dotnet tool update --global dotnet-ef || dotnet tool install --global dotnet-ef
