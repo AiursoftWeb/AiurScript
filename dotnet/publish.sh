2031e21897c90c7e1094a1a0bbfce4ba65675282
Anduin Xue <anduin@aiursoft.com>
2021-12-12 18:38:41 +0800

    Update publish.sh

--- dotnet/publish.sh
+++ dotnet/publish.sh
@@ -3,7 +3,7 @@ publish()
     output="$1"
     csproj="$2"
     ls | grep -q obj || dotnet restore $csproj -r linux-x64
-    dotnet publish -r linux-x64 -c Release --no-self-contained --no-restore -o $output /p:PublishReadyToRun=true $csproj
+    dotnet publish -c Release --no-self-contained --no-restore -o $output $csproj
 }
 
 publish "$@"
