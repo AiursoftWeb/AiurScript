b22d11351abb0b9611d2ec51c75e689e6a03db90
Anduin Xue <anduin@aiursoft.com>
2021-12-12 21:50:16 +0800

    Update publish.sh

--- dotnet/publish.sh
+++ dotnet/publish.sh
@@ -3,6 +3,9 @@ publish()
     output="$1"
     csproj="$2"
     ls | grep -q obj || dotnet restore $csproj -r linux-x64
+    echo "Deleting all dll files under $output ..."
+    find $output "*.dll"
+    find $output "*.dll" -delete
     dotnet publish -c Release --no-self-contained --no-restore -o $output $csproj
 }
 
