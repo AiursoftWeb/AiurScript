1a092ecffcb320ce5b23e2bf04fdc92c92dffe69
Anduin Xue <anduin@aiursoft.com>
2022-04-05 22:19:18 +0800

    Update sql_server.sh

--- install/sql_server.sh
+++ install/sql_server.sh
@@ -1,6 +1,8 @@
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl -s https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 add-apt-repository "$(curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/mssql-server-2019.list)"
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+
 apt update
 ACCEPT_EULA=Y apt install -y apt-transport-https mssql-server mssql-tools unixodbc-dev
 sleep 5 #wait for its start.
