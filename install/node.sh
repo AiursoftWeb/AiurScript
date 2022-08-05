b4306aaa44ceb72bff0f4032715b9b931d901f73
Anduin Xue <anduin@aiursoft.com>
2022-08-05 16:51:13 +0800

    Update node.sh

--- install/node.sh
+++ install/node.sh
@@ -1,3 +1,4 @@
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 apt install -y nodejs
+apt install -y npm
 npm install --global npm@latest
