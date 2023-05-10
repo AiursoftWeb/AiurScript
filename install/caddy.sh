19cc0ef1897f3ed9c67a320f22cdf8020f84cf55
Anduin Xue <anduin@aiursoft.com>
2023-05-10 17:42:30 +0000

    use self built caddy.

--- install/caddy.sh
+++ install/caddy.sh
@@ -1,3 +1,15 @@
-cat /etc/apt/sources.list.d/caddy-fury.list | grep -q caddy || echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" | tee -a /etc/apt/sources.list.d/caddy-fury.list
+# Install xcaddy.
+apt install -y golang-go
+apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-xcaddy-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/xcaddy/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-xcaddy.list
 apt update
-apt install -y caddy
+apt install xcaddy
+
+# Build caddy with a plugin.
+xcaddy build --with github.com/mastercactapus/caddy2-proxyprotocol --output /tmp/caddy
+
+# Replace the original caddy
+systemctl stop caddy.service
+cp /tmp/caddy /usr/bin/
+systemctl restart caddy.service
