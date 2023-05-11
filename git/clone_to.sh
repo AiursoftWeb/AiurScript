9a561f03c5b66ab0759d965b63ad9103ceab7511
Anduin Xue <anduin@aiursoft.com>
2023-05-11 19:40:56 +0000

    New clone to with full URL.

--- git/clone_to.sh
+++ git/clone_to.sh
@@ -2,19 +2,10 @@ clone_to()
 {
     repo="$1"
     path="$2"
-    branch_name=$3;
 
-    if [[ $branch_name == "" ]];
-    then
-        branch_name="master"
-        echo "Using branch name: $branch_name"
-    else
-        echo "Branch name is: $branch_name"
-    fi
-    
     _=$(git --help || apt install git -y)
-    _=$(ls $path && rm $path -rvf)
-    git clone -b $branch_name https://github.com/$repo.git $path
+    _=$(ls $path && rm $path -rf)
+    git clone $repo $path
 }
 
 clone_to "$@"
