44b45c6d40ec1068fd05b45e28565516566180bd
Anduin Xue <anduin@aiursoft.com>
2023-05-12 17:18:48 +0000

    New docker.

--- install/docker.sh
+++ install/docker.sh
@@ -1,6 +1,13 @@
-apt-get install ca-certificates gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get DEBIAN_FRONTEND=noninteractive install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
