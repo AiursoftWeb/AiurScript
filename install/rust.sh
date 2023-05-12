31ee822ba52ef1b0d5fc23b8e454e6f92fe4759a
Anduin Xue <anduin@aiursoft.com>
2023-05-12 13:08:42 +0000

    Fix cargo install.

--- install/rust.sh
+++ install/rust.sh
@@ -1,4 +1,5 @@
-apt autoremove cargo --purge
+export DEBIAN_FRONTEND=noninteractive
+DEBIAN_FRONTEND=noninteractive apt autoremove cargo --purge -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs --output - > install_rust.sh
 chmod +x ./install_rust.sh
 ./install_rust.sh -y
