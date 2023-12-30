bb961ca1190c1b7c845408e8daa6f406cb9fe924
xuef <xuef@microsoft.com>
2023-12-30 14:36:10 +0000

    No logo.

--- install/dotnet.sh
+++ install/dotnet.sh
@@ -2,12 +2,14 @@ cat /etc/environment | grep -q "ASPNETCORE_ENVIRONMENT" || echo 'ASPNETCORE_ENVI
 cat /etc/environment | grep -q "DOTNET_CLI_TELEMETRY_OPTOUT" || echo 'DOTNET_CLI_TELEMETRY_OPTOUT="1"' | tee -a /etc/environment
 cat /etc/environment | grep -q "DOTNET_PRINT_TELEMETRY_MESSAGE" || echo 'DOTNET_PRINT_TELEMETRY_MESSAGE="false"' | tee -a /etc/environment
 cat /etc/environment | grep -q "ASPNETCORE_FORWARDEDHEADERS_ENABLED" || echo 'ASPNETCORE_FORWARDEDHEADERS_ENABLED="true"' | tee -a /etc/environment
+cat /etc/environment | grep -q "DOTNET_NOLOGO" || echo 'DOTNET_NOLOGO="1"' | tee -a /etc/environment
 export DOTNET_PRINT_TELEMETRY_MESSAGE="false"
 export ASPNETCORE_ENVIRONMENT="Production"
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export ASPNETCORE_FORWARDEDHEADERS_ENABLED="true"
+export DOTNET_NOLOGO="1"
 
 apt install -y apt-transport-https
-DOTNET_CLI_TELEMETRY_OPTOUT=1 DOTNET_PRINT_TELEMETRY_MESSAGE="false" apt install -y dotnet7
+DOTNET_CLI_TELEMETRY_OPTOUT=1 DOTNET_PRINT_TELEMETRY_MESSAGE="false" DOTNET_NOLOGO="1" apt install -y dotnet7
 
 #dotnet tool update --global dotnet-ef || dotnet tool install --global dotnet-ef
