04715b01d954a314023f0fa04c74c10b361d7fbb
AnduinXue <anduin@aiursoft.com>
2024-01-23 16:44:28 +0000

    Delete more harder

--- uninstall/docker.sh
+++ uninstall/docker.sh
@@ -1,3 +1,11 @@
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+docker volume rm $(docker volume ls -q)
+docker network rm $(docker network ls -q)
+docker system prune -a -f
+docker system prune --volumes -f
+
 DEBIAN_FRONTEND=noninteractive apt-get remove -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 rm -rf /etc/apt/keyrings/docker.gpg
 rm -rf /etc/apt/sources.list.d/docker.list
@@ -11,6 +19,7 @@ systemctl stop docker.socket
 systemctl disable docker.service
 systemctl disable docker.socket
 find /etc/systemd/ -name docker* | xargs -L1 rm
+find /var/run/ -name docker* | xargs -L1 rm -rf
 sudo rm /etc/init.d/docker
 systemctl daemon-reload
 systemctl list-units --all | grep docker
