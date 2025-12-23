b838df0e91a1175fe9c40e0fa7b4efcc8fc4b4e8
Anduin Xue <anduin@aiursoft.com>
2025-12-23 09:28:22 +0000

    Node majro to 24.

--- install/node.sh
+++ install/node.sh
@@ -6,7 +6,7 @@ fi
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg --yes
-NODE_MAJOR=21
+NODE_MAJOR=24
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 
 sudo apt update
